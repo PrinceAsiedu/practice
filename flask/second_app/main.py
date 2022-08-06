629e15363979baf1f4117543222a2ebc2134045d
Prince Asiedu <prince14asiedu@gmail.com>
2022-08-06 06:12:25 +0100

    Added .gitignore file

--- flask/second_app/main.py
+++ flask/second_app/main.py
@@ -1,4 +1,5 @@
 # Here is our main application
+from turtle import title
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import DevConfig
@@ -13,12 +14,32 @@ class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
+    posts = db.relationship(
+        'Post',
+        backref='users',
+        lazy='dynamic'
+    )
 
     def __init__(self, username):
         self.username = username
 
     def __repr__(self):
         return "<User: '{}'>".format(self.username)
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime())
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+
+    def __init__(self, title):
+        self.title = title
+    
+    def __repr__(self):
+        return "<Post: '{}'>".format(self.title)
 
 
 @app.route('/')
