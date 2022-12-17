1034b5e2a4968f4133c2d893f09c3111816383ff
Prince Asiedu <prince14asiedu@gmail.com>
2022-12-17 01:52:02 +0000

    practice session1.1

---
+++ eloquentjs/deepcomparison.js
@@ -0,0 +1,29 @@
+function deepEqual(a, b) {
+    if (a === b) return true;
+    
+    if (a == null || typeof a!= "object" || 
+        b == null || typeof b != "object" ) return false;
+    
+    let keysA = Object.keys(a), keysB = Object.keys(b);
+    
+    if (keysA.length != keysB.length) return false
+
+    for (let key of keysA) {
+        if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+    }
+
+    return true;
+    
+}
+
+a = {
+    name: 'Prince',
+    age: '24'
+};
+
+b = {
+    name: 'Abigail',
+    age: '19'
+}
+
+console.log(deepEqual(a, b));
