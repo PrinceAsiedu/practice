833c5cc50f68376eeef95c6f15941e6a72954bd1
Prince Asiedu <prince14asiedu@gmail.com>
2022-12-22 01:28:21 +0000

    Added charScript.js

---
+++ eloquentjs/chapter5/charScript.js
@@ -0,0 +1,14 @@
+require('./scripts');
+
+function charScript(code) {
+    for (let script of SCRIPTS) {
+        if (script.ranges.some(([from, to]) => {
+            return code >= from && code < to;
+        })) {
+            return script;
+        }
+    }
+    return null;
+}
+
+console.log(charScript(121));
